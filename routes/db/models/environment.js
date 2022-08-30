62b26b435ecfa1e1795767067868a3dd69cb48d5
flame91 <52809844+flame91@users.noreply.github.com>
2022-08-31 00:20:32 +0900

    Update environment.js

--- routes/db/models/environment.js
+++ routes/db/models/environment.js
@@ -2,11 +2,11 @@ const LOG = false; // true or false
 
 const environments = {
     main: {
-        username: "fallingup",
-        password: "dlfdmscnlal!#%",
-        database: "fallingup",
+        username: "",
+        password: "",
+        database: "",
         dialect: "mysql",
-        host: "175.113.135.86",
+        host: "",
         port: "3306",
         timezone: "+09:00",
         logging: LOG,
